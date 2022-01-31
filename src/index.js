9ed442fa2425a1605738dac4131b7f1ba9ad1a30
Jeremiah Newman <jeremiah.n.a.newman@gmail.com>
2022-01-31 16:52:45 -0500

    read a collection from Firestore

--- src/index.js
+++ src/index.js
@@ -1,25 +1,42 @@
-//Q. how many lines of code does it take to make an api?  A. 3
+const { initializeApp, cert } = require("firebase-admin/app");
+const { getFirestore } = require("firebase-admin/firestore");
+const credentials = require("../credentials.json");
 
-const express = require('express');
+initializeApp({
+  credential: cert(credentials),
+});
+
+const db = getFirestore();
+const usersRef = db.collection("users");
+
+//Q. how many lines of code does it take to make an api? | A. 3
+
+const express = require("express");
 
 const app = express();
 app.use(express.json());
 
-//param 1 is always request; param2 is always res
-app.get('/', (req, res) =>{
-    res.send('Hello World'); 
-})
+//param 1 is always request; param2 is always respond
+app.get("/", (req, res) => {
+ usersRef
+    .get()
+    .then(snapshot => {
+        res.send(snapshot.docs)
+    })
 
-app.post('/users', (req, res) => {
-    const {name, age, email} = req.body; //whatever you send to the server you always send in body
-    //const name = req.body.name
-    //const age = req.body.name
-    const user = {name, age, email};
-    const result = `My name is ${user.name}, I am ${user.age} years old and my email is ${user.email}`;
+//   res.send("Hello World");
+});
 
-    res.send(result);
-})
+app.post("/users", (req, res) => {
+  const { name, age, email } = req.body; //whatever you send to the server you always send in body
+  //const name = req.body.name
+  //const age = req.body.name
+  const user = { name, age, email };
+  const result = `My name is ${user.name}, I am ${user.age} years old and my email is ${user.email}`;
+
+  res.send(result);
+});
 
 app.listen(3000, () => {
-    console.log('Listening on 3000');
-})
+  console.log("Listening on 3000");
+});
